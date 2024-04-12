17f84aabeb61126a2d69f698ca543e592fd3bfdd
Eric Sohel <90656205+esohel30@users.noreply.github.com>
2024-04-12 06:41:44 -0400

    Update tweet_generator.py
    cleaned up

--- app/tweet_generator.py
+++ app/tweet_generator.py
@@ -1,34 +1,13 @@
 from apis import duck, uselessfacts
 import db
 import random
-'''
-Tweet Components:
-1) Profile Picture
-2) Username
-3) Name
-4) Tweet content
-4) Likes
-'''
 
 def generate_pfp():
     pfp = duck.get_duck()
     return pfp
 
-def generate_content_riddle():
-    return riddle.get_url()
-
-def generate_content_uselessfacts():
-    return uselessfacts.get_fact()
-
 def generate_content():
-    # num = random.randint(0,1)
-    # content = None
-    # if (num == 0):
-    #     content = generate_content_riddle()
-    # else:
-    #     content = generate_content_uselessfacts()
-    # return content
-    return generate_content_uselessfacts()
+    return uselessfacts.get_fact()
 
 def generate_tweet(username, name):
     # generates an random tweet that is meant to simulate tweets from the outside world
