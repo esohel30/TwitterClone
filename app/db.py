87d504d2fac78082c9585bafba19d9dbd91fd8ca
Eric Sohel <90656205+esohel30@users.noreply.github.com>
2024-04-12 04:54:42 -0400

    Update db.py
    Code reformat.

--- app/db.py
+++ app/db.py
@@ -13,20 +13,12 @@ def db_close():
     db.close()
 
 def tweet_table_init():
-    '''
-    Tweet Components:
-    1) Profile Picture
-    2) Username
-    3) Name
-    4) Tweet content
-    4) Likes
-    '''
     c =  db_connect()
     c.execute("CREATE TABLE IF NOT EXISTS tweets (pfp text, username text, name text, content text, likes int)")
     db_close()
 
 def create_tweet(pfp, username, name, content, likes):
-    tweet_table_init() # make sure table exists
+    tweet_table_init() 
     c = db_connect()
     c.execute('INSERT INTO tweets VALUES (?, ?, ?, ?, ?)', (pfp, username , name , content , likes))
     db_close()
@@ -43,10 +35,9 @@ def db_user_init():
     c.execute("CREATE TABLE IF NOT EXISTS users (username text, password text, name text)")
     db_close()
 
-## Refactored from project p00 by Rice Explosion (Nakib Abedin, Donald Bi, Wilson Mach)
 
-def check_user_not_exists(username): #checks if user doesn't exist, returns True if they don't exist
-    db_user_init() # make sure table exists
+def check_user_not_exists(username):
+    db_user_init() 
     c = db_connect()
     c.execute('SELECT username FROM users WHERE username=?',(username,))
     user = c.fetchone()
@@ -55,15 +46,15 @@ def check_user_not_exists(username): #checks if user doesn't exist, returns True
         return False
     return True
 
-#for signing up
-def create_new_user(username, password, name): #creates new user
+
+def create_new_user(username, password, name): 
     c = db_connect()
     c.execute('INSERT INTO users VALUES (?,?,?)',(username, password, name))
     c.execute("SELECT * from users")
     db_close()
 
-#for logging in
-def check_credentials(username, password): #checks if there exists username and password in db, returns True if there is
+
+def check_credentials(username, password): 
     c = db_connect()
     c.execute('SELECT username,password FROM users WHERE username=? AND password=?',(username, password))
     user = c.fetchone()
@@ -72,16 +63,10 @@ def check_credentials(username, password): #checks if there exists username and 
         return True
     return False
 
-#given username, returns name
+
 def get_name(username):
     c = db_connect()
     c.execute('SELECT name FROM users WHERE username=?', (username,))
     user = c.fetchone()
     db_close()
     return user[0]
-
-
-
-
-
-
